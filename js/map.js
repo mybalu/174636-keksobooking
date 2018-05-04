f5e772c45b8302957772af10a4b7330f7fb0d50c
Pavel <sorokinpavelvladimirovich@gmail.com>
2018-05-05 01:18:40 +0300

    С pin.js закончил.

--- js/map.js
+++ js/map.js
@@ -1,36 +1,11 @@
 'use strict';
-// Генерирует пины из шаблона и объекта с данными
-var generateMapPin = function (avatarArr) {
-  var mapPin = similarMapPin.cloneNode(true);// это пин <button type="button" class="map__pin" style="..."><img src="..." width="40" height="40" draggable="false" alt="..."></button>
-  var mapPinSpearheadPositionX = window.consts.PIN_WIDTH / 2;// вычисляем, где находится острие метки в зависимости от ее размеров
-  var mapPinSpearheadPositionY = window.consts.PIN_HEIGHT;// вычисляем, где находится острие метки в зависимости от ее размеров
-  // устанавливаем координаты меток
-  var coordinate = window.data.advertsTemplate.location;
-  var coordinateX = coordinate.getX() - mapPinSpearheadPositionX + 'px';
-  var coordinateY = coordinate.getY() - mapPinSpearheadPositionY + 'px';
-
-  mapPin.style.left = coordinateX;
-  mapPin.style.top = coordinateY;
-  mapPin.querySelector('img').setAttribute('src', avatarArr);// аватары авторов на кнопке-пине тоже отрисуем)
-  // Вставляем сделанный пин в фрагмент
-  fragmentForAllPins.appendChild(mapPin);
-};
-
-// Генерируем пины
+var currentAdressInput = document.getElementById('address');
 var formFieldset = document.querySelectorAll('fieldset');
 var map = document.querySelector('.map');
 var pinMain = map.querySelector('.map__pin--main');
-var similarMapPin = document.querySelector('template').content.querySelector('.map__pin');// Это шаблон кнопки-аватара
 var allPins = document.querySelector('.map__pins');// Это блок, куда нужно вставлять все-все готовые метки
-var fragmentForAllPins = document.createDocumentFragment();// Фрагмент, в который вставятся все пины
-
-for (var i = 0; i < window.consts.LANDLORD_COUNT; i++) {
-  generateMapPin(window.data.advertsTemplate.author.getAvatar[i]);
-}
-
-var currentAdressInput = document.getElementById('address');
 var setActivePage = function () {
-  allPins.appendChild(fragmentForAllPins);// вставляем пины в фрагмент
+  allPins.appendChild(window.pin);// вставляем пины в фрагмент
   map.classList.remove('map--faded');// активируем карту
   formFieldset.forEach(function (currentValue) { // включаем поля формы
     currentValue.removeAttribute('disabled');
