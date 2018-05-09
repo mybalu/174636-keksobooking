eeaf1843569a88d83dee880b0988c85f17eb4918
Pavel <sorokinpavelvladimirovich@gmail.com>
2018-05-09 21:31:26 +0300

    Рабочий вариант pin.js сделан. Аватары и координаты получаются по сети и передаются для отрисовки.

--- js/pin.js
+++ js/pin.js
@@ -4,25 +4,27 @@ window.pin = (function () {
   // Генерирует пины из шаблона и объекта с данными
   var fragmentForAllPins = document.createDocumentFragment();// Фрагмент, в который вставятся все пины
   var similarMapPin = document.querySelector('template').content.querySelector('.map__pin');// Это шаблон кнопки-аватара
-  var generateMapPin = function (avatarArr) {
+  var generateMapPin = function (author) {
     var mapPin = similarMapPin.cloneNode(true);// это пин <button type="button" class="map__pin" style="..."><img src="..." width="40" height="40" draggable="false" alt="..."></button>
     var mapPinSpearheadPositionX = window.consts.PIN_WIDTH / 2;// вычисляем, где находится острие метки в зависимости от ее размеров
     var mapPinSpearheadPositionY = window.consts.PIN_HEIGHT;// вычисляем, где находится острие метки в зависимости от ее размеров
     // устанавливаем координаты меток
-    var coordinate = window.data.advertsTemplate.location;
-    var coordinateX = coordinate.getX() - mapPinSpearheadPositionX + 'px';
-    var coordinateY = coordinate.getY() - mapPinSpearheadPositionY + 'px';
+    var coordinateX = author.location.x - mapPinSpearheadPositionX + 'px';
+    var coordinateY = author.location.y - mapPinSpearheadPositionY + 'px';
 
     mapPin.style.left = coordinateX;
     mapPin.style.top = coordinateY;
-    mapPin.querySelector('img').setAttribute('src', avatarArr);// аватары авторов на кнопке-пине тоже отрисуем)
+    mapPin.querySelector('img').setAttribute('src', author.author.avatar);// аватары авторов на кнопке-пине тоже отрисуем)
     // Вставляем сделанный пин в фрагмент
     fragmentForAllPins.appendChild(mapPin);
   };
 
   // Генерируем пины
-  for (var i = 0; i < window.consts.LANDLORD_COUNT; i++) {
-    generateMapPin(window.data.advertsTemplate.author.getAvatar[i]);
-  }
+  window.load(window.consts.DATA_URL, function (ads) {
+    for (var i = 0; i < ads.length; i++) {
+      generateMapPin(ads[i]);
+    }
+    return fragmentForAllPins;
+  }, window.util.showError);
   return fragmentForAllPins;
 })();
